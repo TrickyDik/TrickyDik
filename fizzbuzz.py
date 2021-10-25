aac0e9f5dd086b2b91ff3648ee50840599e86b90
TrickyDik <89946809+TrickyDik@users.noreply.github.com>
2021-10-25 23:11:26 +0100

    FizzBuzz tweaks

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -5,12 +5,12 @@ else if the number is divisible by 3 print Fizz
 else if the number is divisible by 5 print Buzz
 """
 
-for i in range (1,21):
-    if i % 15 == 0:
+for i in range (1,100):
+    if (i % 15) == 0:
         print('FizzBuzz')
-    elif i % 3 == 0:
+    elif (i % 3) == 0:
         print('Fizz')
-    elif i % 5 == 0:
+    elif (i % 5) == 0:
         print('Buzz')
     else:
         print(i)
