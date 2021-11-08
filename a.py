270452317ed3a84ab5227ea2fcb8af6bda4eecc0
TrickyDik <89946809+TrickyDik@users.noreply.github.com>
2021-11-08 21:08:49 +0000

    Ammended Continue function

--- a.py
+++ a.py
@@ -1,16 +1,5 @@
-# user = input('Would you like to continue?')
-# if user.upper !='Y' or 'YES':
-#     print('Exiting')
-# else:
-#     print('Continuing')
-#     print('Complete')
-
-value = input('Would you like to continue? ')
-
-if value == 'y' or value == 'yes':
-    print('Continuing ...')
-    print('Complete!')
-elif value == 'n' or value == 'no':
-    print('Exiting')
-else:
-    print('Please try again and respond with yes or no.')
+#Formatted string
+firstname = input('Enter firstname: ')
+lastname = input('Enter lastname: ')
+message = f'{lastname}, {firstname}'
+print(message)
